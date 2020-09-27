c605977f56d635ef873c93f1bd71ee30c215e1cd
Sandy Wong <fed_up@hotmail.com>
2020-09-27 02:26:15 -0500

    Connected SelectorVC to 2nd VC and auto layout complete

--- HouseNineBallScorer/selectorVCViewController.swift
+++ HouseNineBallScorer/selectorVCViewController.swift
@@ -8,13 +8,35 @@
 
 import UIKit
 
-class selectorVCViewController: UIViewController {
+class selectorVCViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return valueArray.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        nineballValueSelected = Int(valueArray[row])!
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return valueArray[row]
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        valuePicker.dataSource = self
+        valuePicker.delegate = self
+        
     }
+    
+    let valueArray = ["3", "2"]
+    var nineballValueSelected = 2
     
     var player1win = 0
     var player2win = 0
@@ -24,6 +46,7 @@ class selectorVCViewController: UIViewController {
     
     
     
+    @IBOutlet weak var valuePicker: UIPickerView!
     
     @IBAction func player1Slider(_ sender: UISlider) {
         
@@ -43,5 +66,22 @@ class selectorVCViewController: UIViewController {
         player2PointsNeeded.text = String(format: "%.0f", sender.value)
         player2win = Int(sender.value)
     }
+    
+    
+    @IBAction func startBtnPressed(_ sender: UIButton) {
+        
+        performSegue(withIdentifier: "goToGame", sender: self)
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let destinationVC = segue.destination as! ViewController
+        destinationVC.nineballValue = nineballValueSelected
+        print(nineballValueSelected)
+        
+    }
+    
+    
+    
     
 }
